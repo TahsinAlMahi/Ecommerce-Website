d5564d5f1c06093d03da946ff7f451e5730f35be
Tahsin Al Mahi <tahsinalmahi@yahoo.com>
2020-09-13 22:42:57 +0600

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose')
 
-// mongoose.connect("mongodb://127.0.0.1:27017/shop", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true
-// })
-
-mongoose.connect("mongodb://tahsinalmahi:Mahi1994@cluster0.tgcnd.mongodb.net/shop?retryWrites=true&w=majority", {
+mongoose.connect("mongodb://127.0.0.1:27017/shop", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
